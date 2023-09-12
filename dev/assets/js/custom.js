df89bf8dac345ba0f6921c429c0b528994177b65
Vitalii Sukhorukov <Sukhorukov.Vitaliy.S@hneu.net>
2023-09-12 16:40:41 +0300

    advantages

---
+++ dev/assets/js/custom.js
@@ -0,0 +1,39 @@
+const burger = document.querySelector('#hamburger');
+
+burger.addEventListener('click', function(){
+    burger.classList.toggle('is-active')
+})
+
+
+
+$(document).ready(function() {
+  
+      mobileOnlySlider("#advanages-slider", true, false, 1024);
+
+function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+  var slider = $($slidername);
+  var settings = {
+    mobileFirst: true,
+    dots: $dots,
+    arrows: $arrows,
+    responsive: [
+      {
+        breakpoint: $breakpoint,
+        settings: "unslick"
+      }
+    ]
+  };
+
+  slider.slick(settings);
+
+  $(window).on("resize", function () {
+    if ($(window).width() > $breakpoint) {
+      return;
+    }
+    if (!slider.hasClass("slick-initialized")) {
+      return slider.slick(settings);
+    }
+  });
+} // Mobile Only Slider
+});
+
